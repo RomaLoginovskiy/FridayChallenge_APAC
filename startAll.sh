8163e18d1cbb183c86f78dc8cead5ca7df1939b1
unknown <roma.loginovskiy@gmail.com>
2021-03-16 17:20:44 +0100

    Init

--- startAll.sh
+++ startAll.sh
@@ -1,8 +1,3 @@
-export  CORECLR_PROFILER={57e1aa68-2229-41aa-9931-a6e93bbc64d8}
-export  CORECLR_ENABLE_PROFILING=1
-export  CORECLR_PROFILER_PATH=./libappdprofiler.so
-
-
 cd /home/centos/Gateway
 chmod 777 StartGatewayApi.sh
 ./StartGatewayApi.sh
